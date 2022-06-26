ec3996014724cfeba4c0c3a616ab433f317ec903
kmedinamunoz <karen.medina@havas.com>
2022-06-26 11:58:10 -0600

    Uploading files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clase6'`, () => {
+  it(`should have as title 'tarea-3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Challenge');
